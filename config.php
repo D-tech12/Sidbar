a16baf9dd4ae093f5f259fed0af084d8ea50dff5
Hidayatnb <129621260+Hidayatnb@users.noreply.github.com>
2023-04-08 22:26:39 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php 
+$databaseHost = "localhost";
+$databaseName = "dtech";
+$databaseUsername = "root";
+$databasePassword = ''; 
+
+$config = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+
+?>
